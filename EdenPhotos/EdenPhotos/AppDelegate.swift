7f89387f50db16e8d50694d9f57cc46b0b7e4554
Zhivka Ivanova <zhivka.ivanova@edentechlabs.io>
2020-01-28 17:40:18 +0200

    Create project

---
+++ EdenPhotos/EdenPhotos/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  EdenPhotos
+//
+//  Created by Zhivka Ivanova on 28.01.20.
+//  Copyright © 2020 Zhivka Ivanova. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
